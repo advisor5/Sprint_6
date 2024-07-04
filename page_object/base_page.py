f10e81f06cbb81fdcf27ff2cd8ad03c82f4a68de
Sergei Kravchuk <advisor5@yandex.ru>
2024-07-04 12:13:30 +0300

    add new folder: page_object/ with files

---
+++ page_object/base_page.py
@@ -0,0 +1,35 @@
+import allure
+from selenium.webdriver.firefox.webdriver import WebDriver
+from locators.base_page_locators import LocatorsBase
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from constants import Url
+
+
+class BasePage:
+    order_button_header = (LocatorsBase.ORDER_BUTTON_TOP)
+    cookie_button = (LocatorsBase.COOKIE_BUTTON)
+    scooter_botton = (LocatorsBase.SCOOTER_BUTTON)
+    yandex_button = (LocatorsBase.YANDEX_BUTTON)
+
+    def __init__(self,  driver: WebDriver):
+        self.driver = driver
+
+    def wait_cookie_botton(self):
+        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.cookie_button))
+    
+    def click_cookie_botton(self):
+        self.driver.find_element(*self.cookie_button).click()
+    
+    @allure.step('Кликаем на кнопку "Заказать" вверху страницы')
+    def click_on_the_order_button_header(self):
+        self.driver.find_element(*self.order_button_header).click()
+
+    def click_on_the_scooter_button(self):
+        self.driver.find_element(*self.scooter_botton).click()
+    
+    def click_on_the_yandex_button(self):
+        self.driver.find_element(*self.yandex_button).click()
+    
+    def wait_dzen_site(self):
+        WebDriverWait(self.driver, 5).until(EC.url_contains(Url.DZEN_SITE))
