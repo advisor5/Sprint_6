507ce3a85b992d5b297c72b7067ed843732c736f
Sergei Kravchuk <advisor5@yandex.ru>
2024-07-07 11:42:04 +0300

    edit base_page.py

--- page_object/base_page.py
+++ page_object/base_page.py
@@ -15,9 +15,11 @@ class BasePage:
     def __init__(self,  driver: WebDriver):
         self.driver = driver
 
+    @allure.step('Ожидаем появления уведомления об использовании куков')
     def wait_cookie_botton(self):
         WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.cookie_button))
     
+    @allure.step('Кликаем на кнопку "Да все привыкли"')
     def click_cookie_botton(self):
         self.driver.find_element(*self.cookie_button).click()
     
@@ -25,11 +27,14 @@ class BasePage:
     def click_on_the_order_button_header(self):
         self.driver.find_element(*self.order_button_header).click()
 
+    @allure.step('Кликаем на лого "Самокат" вверху страницы')
     def click_on_the_scooter_button(self):
         self.driver.find_element(*self.scooter_botton).click()
     
+    @allure.step('Кликаем на лого "Яндекс" вверху страницы')
     def click_on_the_yandex_button(self):
         self.driver.find_element(*self.yandex_button).click()
-    
+        
+    @allure.step('Ожидаем загрузку главной страницы Дзена')
     def wait_dzen_site(self):
         WebDriverWait(self.driver, 5).until(EC.url_contains(Url.DZEN_SITE))
