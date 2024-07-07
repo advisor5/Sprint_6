f2fb6d01f1bb5cf3bb4256632345bc3bdcc4a711
Sergei Kravchuk <advisor5@yandex.ru>
2024-07-07 11:22:44 +0300

    edit locators files: edit locators

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -2,8 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class LocatorsBase:
-    ORDER_BUTTON_TOP = (By.XPATH, "//div[@class='Header_Nav__AGCXC']/button[text()='Заказать']") # Кнопка Заказать в хедере
+    ORDER_BUTTON_TOP = (By.XPATH, "//div[contains(@class,'Header_Nav')]/button[text()='Заказать']") # Кнопка Заказать в хедере
     SCOOTER_BUTTON = (By.XPATH, "//img[@src='/assets/scooter.svg']") # Логотип Самокат
-    YANDEX_BUTTON = (By.CLASS_NAME, "Header_LogoYandex__3TSOI") # Логотип Яндекс
+    YANDEX_BUTTON = (By.XPATH, "//a[contains(@class,'Header_LogoYandex')]") # Логотип Яндекс
     COOKIE_BUTTON = (By.XPATH, "//button[@id='rcc-confirm-button']") # Окно с куки
-
--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 class LocatorsOrder:
     # Страница 1 заказа
-    ORDER_HEADER_TITLE = (By.CLASS_NAME, "Order_Header__BZXOb") # Заголовок на странице зказа //div[text()='Для кого самокат']
+    ORDER_HEADER_TITLE = (By.XPATH, "//div[contains(@class,'Order_Header')]") # Заголовок на странице зказа
     FIELD_NAME = (By.XPATH, "//input[@placeholder='* Имя']") #  Поле Имя
     FIELD_SURNAME = (By.XPATH, "//input[@placeholder='* Фамилия']") #  Поле Фамилия
     FIELD_ADDRESS = (By.XPATH, "//input[@placeholder='* Адрес: куда привезти заказ']") #  Поле Адрес
@@ -15,7 +15,7 @@ class LocatorsOrder:
     TITLE_ABOUT_RENT = (By.XPATH, "//div[text()='Про аренду']") # Залоговок Про аренду
     FIELD_WHEN_DELIVERY = (By.XPATH, "//input[@placeholder='* Когда привезти самокат']") # Выбор даты доставки
     FIELD_DAYS_RENT = (By.XPATH, "//div[contains(@class,'Dropdown-root')]") # Поле для выбора дней аренды
-    ORDER_BOTTON = (By.XPATH, "//div[@class='Order_Buttons__1xGrp']//button[text()='Заказать']") #  Кнопка Заказать
+    ORDER_BOTTON = (By.XPATH, "//div[contains(@class,'Order_Buttons')]/button[text()='Заказать']") #  Кнопка Заказать
     WINDOWS_WANT_ORDER = (By.XPATH, "//div[text()='Хотите оформить заказ?']") # Окно - Хотите оформить заказ
     YES_BUTTON = (By.XPATH, "//button[text()='Да']") # Кнопка ДА
     ORDER_IS_PROCESSED = (By.XPATH, "//div[contains(@class,'Order_ModalHeader')]") # Заказ оформлен
