f26e5fdaa6e7bd574dc13d0827e440d934adfb01
Sergei Kravchuk <advisor5@yandex.ru>
2024-07-07 11:39:05 +0300

    edit conftest.py: add fixtures

--- conftest.py
+++ conftest.py
@@ -1,5 +1,9 @@
 import pytest
 from selenium import webdriver
+from page_object.base_page import BasePage
+from page_object.main_page import MainPage
+from page_object.order_page import OrderPage
+
 from constants import Url
 
 @pytest.fixture
@@ -8,3 +12,18 @@ def driver():
     driver.get(Url.HOST)
     yield driver
     driver.quit()
+
+@pytest.fixture
+def main_page(driver):
+    page = MainPage(driver)
+    return page
+
+@pytest.fixture
+def base_page(driver):
+    base = BasePage(driver)
+    return base
+
+@pytest.fixture
+def order_page(driver):
+    order = OrderPage(driver)
+    return  order
